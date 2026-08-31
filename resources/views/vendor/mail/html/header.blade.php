9304cd655f710a10a39653b75178414f37018e74
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2026-08-31 20:34:44 +0700

    update timezone

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{ asset('images/logo/logo.webp') }}" class="logo" alt="{{ config('app.name') }} Logo" style="max-height: 50px;">
+    {{ config('app.name') }}
 </a>
 </td>
 </tr>
